7fb24c0cd496b6e66e593c9386705d9b2ccab4bf
Hein Htet Kyaw <mr.kiddomonster@gmail.com>
2025-04-10 07:47:25 +0630

    sites: update.

--- _data/sites/sites.js
+++ _data/sites/sites.js
@@ -3,9 +3,9 @@ module.exports = {
   description: "Hein Htet Kyaw's V/Blog",
   // skip if localhost
   // skip if this is a new fork of the speedlify (not Zach’s)
-  skip: !process.env.CONTEXT || process.env.SITE_NAME !== "ksvspeedlify",
+  skip: !process.env.CONTEXT || process.env.SITE_NAME !== "hhkspeedlify",
   options: {
-    frequency: 60 * 11 + 30, // 11h, 30m
+    frequency: 60 * 23, // 24 hours
     // Use "run" if the sites don’t share assets on the same origin
     //           and we can reset chrome with each run instead of
     //           each site in every run (it’s faster)
