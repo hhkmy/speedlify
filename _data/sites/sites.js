47bef580dbdc83a9ab2a23e434f56f5e0c9cd3c8
Hein Htet Kyaw <mr.kiddomonster@gmail.com>
2025-05-11 18:39:38 +0630

    Update sites.js

--- _data/sites/sites.js
+++ _data/sites/sites.js
@@ -5,7 +5,7 @@ module.exports = {
   // skip if this is a new fork of the speedlify (not Zach’s)
   skip: !process.env.CONTEXT || process.env.SITE_NAME !== "hhkspeedlify",
   options: {
-    frequency: 22140, // 369 minutes (6.15 hours)
+    frequency: '9 */6 * * *', // 369 minutes (6.15 hours)
     // Use "run" if the sites don’t share assets on the same origin
     //           and we can reset chrome with each run instead of
     //           each site in every run (it’s faster)
@@ -13,9 +13,9 @@ module.exports = {
     freshChrome: "site",
   },
   urls: [
-    "https://hhk.my.id/",
-    "https://speedlify.hhk.my.id/",
-    "https://stats.hhk.my.id/",
-    "https://ksvblog.pages.dev",
+    "https://hhk.my.id",
+    "https://netlify.hhk.my.id",
+    "https://speedlify.hhk.my.id",
+    "https://stats.hhk.my.id",
   ],
 };
