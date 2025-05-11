a46ca012217ada494f743a9612523c272cb9179b
Hein Htet Kyaw <mr.kiddomonster@gmail.com>
2025-05-11 14:47:54 +0630

    Update: sites.js
    Removed: pages.dev Domain and Change Every 369 Minutes.

--- _data/sites/sites.js
+++ _data/sites/sites.js
@@ -1,11 +1,11 @@
 module.exports = {
   name: "sites",
-  description: "Hein Htet Kyaw's V/Blog",
+  description: "Hein Htet Kyaw's B/Vlog",
   // skip if localhost
   // skip if this is a new fork of the speedlify (not Zach’s)
   skip: !process.env.CONTEXT || process.env.SITE_NAME !== "hhkspeedlify",
   options: {
-    frequency: 60 * 23, // 24 hours
+    frequency: 22140, // 369 minutes (6.15 hours)
     // Use "run" if the sites don’t share assets on the same origin
     //           and we can reset chrome with each run instead of
     //           each site in every run (it’s faster)
@@ -17,6 +17,5 @@ module.exports = {
     "https://speedlify.hhk.my.id/",
     "https://stats.hhk.my.id/",
     "https://ksvblog.pages.dev",
-    "https://ksvblog.netlify.app",
   ],
 };
